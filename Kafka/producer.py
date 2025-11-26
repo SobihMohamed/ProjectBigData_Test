02bac00d157a7c6880f9664a8254e169b6c846e0
Sobih <sobihmohamedsobih@gmail.com>
2025-11-26 23:11:41 +0200

    add nifi and dataset

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -1,35 +1,47 @@
 from kafka import KafkaProducer
 import json
-import random
+import csv
 import time
 
-class EnergyProducer:
-  def __init__(self , topic='energy_reading' , bootstrap_servers='localhost:9092' , devices_file='devices.json'):
-    self.producer = KafkaProducer(
-      bootstrap_servers = bootstrap_servers,
-      #value_serializer → بيحوّل أي بيانات Python (dictionary) إلى JSON string ويشفّرها byte string قبل الإرسال، لأن Kafka بيقبل بس byte.
-      value_serializer = lambda v: json.dumps(v).encode('utf-8')
-    )
-    self.topic = topic
+class FileProducer:
+    def __init__(self, bootstrap_servers='localhost:9092'):
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8') 
+        )
 
-    with open(devices_file,'r') as f:
-      self.devices = json.load(f)['devices']
+    def send_csv_to_kafka(self, file_path, topic_name, limit=100):
+        print(f"Starting to send data from {file_path} to topic: {topic_name}")
+        
+        with open(file_path, 'r', encoding='utf-8') as f:
 
-  def generateData(self) :
-    return {
-      'devices' : random.choice(self.devices),
-      'consumption' : random.randint(50,500),
-      'timestamp': time.strftime('%Y-%m-%d %H:%M:%S')
-    }
-  
-  def sendData(self,num_message=10,interval=1):
-    for _ in range(num_message):
-      data = self.generateData()
-      self.producer.send(self.topic,value=data)
-      print(f"Sent: {data}")
-      time.sleep(interval)
-    self.producer.flush()
+            reader = csv.DictReader(f)
+            
+            count = 0
+            for row in reader:
+                # senf to kafka
+                self.producer.send(topic_name, value=row)
+                print(f"Sent to {topic_name}: {row}")
+                
+                # for streaming 
+                time.sleep(0.5) 
+                
+                count += 1
+                # لو حاطط ليميت معين عشان التجربة
+                if limit > 0 and count >= limit:
+                    break
+        
+        self.producer.flush()
+        print(f"Finished sending {count} records to {topic_name}")
 
 if __name__ == "__main__":
-  producer = EnergyProducer()
-  producer.sendData(num_message=20 , interval=1)
+    producer = FileProducer()
+
+    path_file_1 = '../DataSet/Data_Set_1_DeviceMeasurment.csv'
+    path_file_2 = '../DataSet/Data_Set_2_Flags.csv'
+
+    input("Press Enter to start sending File 1...")
+    producer.send_csv_to_kafka(file_path=path_file_1, topic_name='topic-file-1')
+
+    input("Press Enter to start sending File 2...")
+    producer.send_csv_to_kafka(file_path=path_file_2, topic_name='topic-file-2')
