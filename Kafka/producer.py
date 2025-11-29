b92e0b47d3414166e37135431b24a95038995855
Sobih <sobihmohamedsobih@gmail.com>
2025-11-29 22:48:21 +0200

    Modfying the Producer and NIFI and Create Spark Code

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -2,46 +2,58 @@ from kafka import KafkaProducer
 import json
 import csv
 import time
+import os
 
-class FileProducer:
+class Producer:
     def __init__(self, bootstrap_servers='localhost:9092'):
         self.producer = KafkaProducer(
             bootstrap_servers=bootstrap_servers,
-            value_serializer=lambda v: json.dumps(v).encode('utf-8') 
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
         )
 
-    def send_csv_to_kafka(self, file_path, topic_name, limit=100):
-        print(f"Starting to send data from {file_path} to topic: {topic_name}")
+    def start_streaming(self, file1_path, file2_path, topic1, topic2 , limit = 50):
+        print(f"---Starting Stream from:\n 1. {file1_path}\n 2. {file2_path}\n---")
         
-        with open(file_path, 'r', encoding='utf-8') as f:
+        try:
+            with open(file1_path, 'r', encoding='utf-8') as f1 , open(file2_path, 'r', encoding='utf-8') as f2:
+                reader1 = csv.DictReader(f1)
+                reader2 = csv.DictReader(f2)
+                
+                counter = 0
+                
+                for r1, r2 in zip(reader1, reader2):
+                    
+                    if 'Device' not in r1 or not r1['Device']:
+                      r1['Device'] = 'Unknown_Device'
+                    
+                    r2['Timestamp'] = r1['Timestamp']
+                    r2['House_ID'] = r1['House_ID']
 
-            reader = csv.DictReader(f)
+                    self.producer.send(topic1, value=r1)
+                    print(f"Send to topic name : {topic1} = {r1}")
+                    self.producer.send(topic2, value=r2)
+                    print(f"Send to topic name : {topic2} = {r2}")
+                    
+                    counter += 1
+                    if counter > limit :
+                      break
+                    
+                    time.sleep(3)
             
-            count = 0
-            for row in reader:
-                # senf to kafka
-                self.producer.send(topic_name, value=row)
-                print(f"Sent to {topic_name}: {row}")
-                
-                # for streaming 
-                time.sleep(0.5) 
-                
-                count += 1
-                # لو حاطط ليميت معين عشان التجربة
-                if limit > 0 and count >= limit:
-                    break
-        
-        self.producer.flush()
-        print(f"Finished sending {count} records to {topic_name}")
+            self.producer.flush()
+            print("\n --- Done Sending All Data ---")
+
+        except FileNotFoundError:
+            print(f"\nError In file Path")
 
 if __name__ == "__main__":
-    producer = FileProducer()
+    producer = Producer()
 
-    path_file_1 = '../DataSet/Data_Set_1_DeviceMeasurment.csv'
-    path_file_2 = '../DataSet/Data_Set_2_Flags.csv'
-
-    input("Press Enter to start sending File 1...")
-    producer.send_csv_to_kafka(file_path=path_file_1, topic_name='topic-file-1')
-
-    input("Press Enter to start sending File 2...")
-    producer.send_csv_to_kafka(file_path=path_file_2, topic_name='topic-file-2')
+    path_1 = '../DataSet/Data_Set_1_DeviceMeasurment.csv'
+    path_2 = '../DataSet/Data_Set_2_Flags.csv'
+    
+    topic_1 = 'topic-file-1'
+    topic_2 = 'topic-file-2'
+    
+    input("Press Enter to start streaming...")
+    producer.start_streaming(path_1, path_2, topic_1, topic_2)
--- Spark/Streaming_job.py
+++ Spark/Streaming_job.py
@@ -1,50 +1,127 @@
+import os
+import time
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1 pyspark-shell'
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.functions import from_json, col, explode, avg, max, min, to_timestamp, expr, trim
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, ArrayType
 
-class EnergyStreamingJob:
-  def __init__(self , kafka_bootstrap='localhost:9092', topic='energy_reading', db_url=None):
-    #define variables
-    self.kafka_bootstrap = kafka_bootstrap
-    self.topic = topic
-    self.db_url = db_url
+class EnergyProject:
+    def __init__(self):
+        self.spark = SparkSession.builder \
+            .appName("EnergyFinalFixed") \
+            .master("local[*]") \
+            .config("spark.sql.shuffle.partitions", "2") \
+            .config("spark.sql.legacy.timeParserPolicy", "LEGACY") \
+            .getOrCreate()
+        
+        
+        self.record_schema = StructType([
+            StructField("Timestamp", StringType(), True),
+            StructField("House_ID", StringType(), True),
+            StructField("Room", StringType(), True),
+            StructField("Device", StringType(), True),
+            StructField("Device_Status", StringType(), True),
+            StructField("Power_Usage_W", StringType(), True),
+            StructField("Voltage_V", StringType(), True),
+            StructField("Current_A", StringType(), True), 
+            StructField("Energy_kWh", StringType(), True),
+            StructField("Peak_Hours_Flag", StringType(), True),
+            StructField("Sudden_Increase_Flag", StringType(), True),
+            StructField("Temperature_C", StringType(), True)
+        ])
 
-    #Create spark session 
-    self.SparkSession = SparkSession.builder.appName("EnergyStreaming")\
-      .config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1").getOrCreate()
+    def print_device_stats(self, df, epoch_id):
+        if df.count() > 0:
+            print(f"\n📊 [QUERY 1] Device Statistics | Batch: {epoch_id}")
+            time.sleep(1)
+            df.show(truncate=False) 
+            time.sleep(1)
 
-    #create Schema
-    self.Schema = StructType([
-      StructField("devices",StringType(),True),
-      StructField("consumption",IntegerType(),True),
-      StructField("timestamp",StringType(),True),
-    ])
+    def print_alerts(self, df, epoch_id):
+        if df.count() > 0:
+            print("\n" + "!"*60)
+            print(f"🚨 [QUERY 2] ALERT DETECTED (Device Identified!) | Batch: {epoch_id}")
+            time.sleep(1)
+            print("!"*60)
+            df.show(truncate=False)
+            time.sleep(1)
+        else:
+            print("\n NO ALERT DETECTED\n")
+    
+    def start_pipeline(self):
+        print(">>> Starting Pipeline (Legacy Mode)...")
 
-  def start_streaming(self):
-    #read from the kafka which has the value in binary
-    dataFromKafka = self.SparkSession.readStream\
-      .format("kafka")\
-      .option("kafka.bootstrap.servers",self.kafka_bootstrap)\
-      .option("subscribe",self.topic)\
-      .option("startingOffsets","earliest")\
-      .load()
-    #and the change it to string 
-    #and then change it into Schema which i Created
-    dataToSavedInDataFrame = dataFromKafka.selectExpr("CAST(value AS STRING) as raw_data") \
-                                .select(from_json(col("raw_data"), self.Schema).alias("data")) \
-                                .select("data.*")
+        # 1. Ingestion
+        raw_stream = self.spark.readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "localhost:9092") \
+            .option("subscribe", "topic-processed-data") \
+            .option("startingOffsets", "latest") \
+            .load()
 
-    # create some Analysis 
-    dataToSavedInDataFrame.groupBy("devices") \
-      .avg("consumption") \
-      .writeStream \
-      .outputMode("complete") \
-      .format("console") \
-      .option("checkpointLocation", "D:/spark_checkpoint") \
-      .start() \
-      .awaitTermination()
+        # 2. Parsing (Explode Array)
+        json_stream = raw_stream.selectExpr("CAST(value AS STRING) as json_string")
 
-      
+        parsed_array = json_stream.select(
+            from_json(col("json_string"), ArrayType(self.record_schema)).alias("data_array")
+        )
+
+        exploded_stream = parsed_array.select(explode(col("data_array")).alias("data"))
+        
+        flat_stream = exploded_stream.select("data.*")
+
+        # 3. Cleaning & Timestamp Parsing
+        # M/d/yyyy H:mm -> بيقبل 1/1/2025 0:00 (بفضل الـ Legacy Mode)
+        # trim -> عشان لو فيه مسافة زيادة في الأول او الآخر
+        clean_df = flat_stream \
+            .withColumn("EventTime", to_timestamp(trim(col("Timestamp")), "M/d/yyyy H:mm")) \
+            .withColumn("Power_Usage_W", col("Power_Usage_W").cast(DoubleType())) \
+            .withColumn("Sudden_Increase_Flag", col("Sudden_Increase_Flag").cast(IntegerType()))
+
+        # 4. Data Splitting & Watermarking
+        measurements_df = clean_df.filter(col("Device").isNotNull()) \
+            .select("EventTime", "House_ID", "Device", "Power_Usage_W") \
+            .withWatermark("EventTime", "20 minutes")
+        
+        flags_df = clean_df.filter(col("Sudden_Increase_Flag") == 1) \
+            .select("EventTime", "House_ID" , "Sudden_Increase_Flag") \
+            .withWatermark("EventTime", "20 minutes")
+        
+        # 5. JOIN
+        joined_alerts = flags_df.alias("f").join(
+            measurements_df.alias("m"),
+            expr("f.House_ID = m.House_ID AND f.EventTime = m.EventTime")
+        ).select(
+            col("f.EventTime"),
+            col("f.House_ID"),
+            col("m.Device"),
+            col("m.Power_Usage_W"),
+            col("f.Sudden_Increase_Flag")
+        )
+        
+        joined_alerts = joined_alerts.dropDuplicates(["EventTime", "House_ID"])
+
+        # 6. Outputs
+        query1 = measurements_df \
+            .groupBy("Device") \
+            .agg(
+                avg("Power_Usage_W").alias("Avg_Power"),
+                max("Power_Usage_W").alias("Max_Consumption"),
+                min("Power_Usage_W").alias("Min_Consumption"),
+            ) \
+            .writeStream \
+            .outputMode("complete") \
+            .foreachBatch(self.print_device_stats) \
+            .start()
+
+        query2 = joined_alerts.writeStream \
+            .outputMode("append") \
+            .foreachBatch(self.print_alerts) \
+            .start()
+
+        self.spark.streams.awaitAnyTermination()
+
 if __name__ == "__main__":
-    job = EnergyStreamingJob()
-    job.start_streaming()
+    project = EnergyProject()
+    project.start_pipeline()
